685b40a5fdeeec450a047bd4996821f4999c072b
Eden <115366725+eden-sobol@users.noreply.github.com>
2023-02-26 18:36:54 -0500

    Add files via upload

---
+++ polyfusion.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.body.innerHTML += poly.html.body
+    document.head.innerHTML += poly.html.head
+    document.head.innerHTML += "<style id='style'></style>"
+    document.getElementById("style").innerHTML += poly.css
+}
---
+++ script.js
@@ -0,0 +1,15 @@
+const poly = {
+    html: {
+        body: `
+        <p>hi</p>
+        `,
+        head: `
+        <title>test</title>
+        `
+    },
+    style: `
+    p {
+        color: red;
+    }
+    `
+}
