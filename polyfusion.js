1a381bbebd66d44e7dd05f5324030c5a4c3325c7
Eden <115366725+eden-sobol@users.noreply.github.com>
2023-03-05 20:57:34 -0600

    Update polyfusion.js

--- polyfusion.js
+++ polyfusion.js
@@ -1,6 +1,10 @@
+function code(code) {
+    setTimeout(code)
+}
+
 window.onload = function() {
     document.body.innerHTML += poly.html.body
     document.head.innerHTML += poly.html.head
     document.head.innerHTML += "<style id='style'></style>"
-    document.getElementById("style").innerHTML += poly.css
+    document.getElementById("style").innerHTML += poly.style
 }
